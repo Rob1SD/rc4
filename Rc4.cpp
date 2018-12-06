679b6e921f1fd1e3b48e5c5c08c58fa3472d5791
robin <robin.solde@gmail.com>
2018-12-06 21:04:11 +0100

    je comprends rien

--- Rc4.cpp
+++ Rc4.cpp
@@ -5,31 +5,48 @@
 
 
 #include "Rc4.h"
+std::ifstream::pos_type filesize(const char* filename)
+{
+    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+    return in.tellg();
+}
+
 
 Rc4::Rc4(char *filein, char *fileout, unsigned char *key) : filein(filein), fileout(fileout), key(key) {
-    std::ifstream fichier(filein);
-    std::string ligne;
-    std::stringstream tmpStream;
-    while (getline(fichier, ligne)) // attentions sur des gros fichier ca explose la ram mais ca ira pour demain;
-    {
+//    std::ifstream fichier(filein);
+//    std::string ligne;
+//    std::stringstream tmpStream;
+//    while (getline(fichier, ligne)) // attentions sur des gros fichier ca explose la ram mais ca ira pour demain;
+//    {
 //        unsigned char tmp[std::strlen((char *)inString) + ligne.length()];
 //        unsigned long sizeLigne=ligne.length();
 //        char tab[sizeLigne];
 //        strcpy(tab,ligne.c_str());
 //        std:strcat((char*)tmp, tab);
 //        inString = tmp;
-        std::cout << "ligne" << std::endl;
-        std::cout << ligne << std::endl;
-        tmpStream << ligne << std::endl;
-    }
-    std::cout << "fichier" << std::endl;
-    std::cout << tmpStream.str() << std::endl;
-    inString=(unsigned char*)tmpStream.str().c_str();
+//        std::cout << "ligne" << std::endl;
+//        std::cout << ligne << std::endl;
+//        tmpStream << ligne << "\n";
+//    }
+//    std::cout << "fichier" << std::endl;
+//    std::cout << tmpStream.str() << std::endl;
+//    std::string tmpstr= tmpStream.str().substr(0, tmpStream.str().length());
+//    std::cout << "tmpStream.str() : " << tmpStream.str() << std::endl;
+//    std::cout << "tmpstr : " << tmpstr << std::endl;
+//    inString=(unsigned char*)tmpstr.c_str();
+//    std::cout << "inString0 : " << (std::string)((char*)inString) << std::endl;
     //prga();
+
+    long long int fileSize = filesize(filein);
+    FILE *fichier = fopen(filein, "rb");
+    unsigned char content[fileSize];
+    fread(content, sizeof(unsigned char), fileSize, fichier);
+    std::cout << "content" << content << std::endl;
+    inString=content;
+//    prga();
 }
 
-
-void Rc4::ksa() {
+    void Rc4::ksa() {
     for (int i = 0; i < 256; ++i)
     {
         s[i]=(unsigned char) i;
@@ -50,24 +67,32 @@ void Rc4::prga()
 {
     int i = 0;
     int j = 0;
+    std::cout << "instring : " << inString << std::endl;
     unsigned char out[std::strlen((char*) inString)];
     for (int k = 0; k < std::strlen((char*)inString); ++k)
     {
         i = (i+1)%256;
         j = (j+s[i])%256;
         unsigned char tmp = s[i];
+        std::cout << "'" << (int)((char)s[i]) << "'"<<std::endl;
         s[i] = s[j];
         s[j] = s[tmp];
         out[k]=s[(s[i]+s[j])%256]^inString[k];
+        std::cout << "'" << (int)((char)out[k]) << "'"<<std::endl;
 //        std::cout << "babibel" << std::endl;
 //        std::cout << (int) out[k] << std::endl;
     }
     outString=out;
     std::string res((char*)out);
     std::cout << "conversion finie" << std::endl;
-    std::ofstream fichierOut(fileout);
-    fichierOut.write((char*)&out, sizeof out);
+//    std::ofstream fichierOut(fileout);
+//    for (int i = 0; i < 30; ++i)
+//        std::cout << "'" << (int)((char)out[i]) << "'"<<std::endl;
+//    fichierOut.write((char*)&out, sizeof out);
 //    std::cout << res << std::endl;
+    long long int fileSize = filesize(filein);
+    FILE *fichier = fopen(fileout, "wb");
+    fwrite(outString, sizeof(unsigned char), fileSize, fichier);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <cstring>
 #include "Rc4.h"
-int main() {
+int main(int argc, char *argv[]) {
 
     std::string str("titi");
-//    Rc4 *rc4 = new Rc4("/home/robin/CLionProjects/RC4/testIn.txt", "/home/robin/CLionProjects/RC4/testOut.txt", (unsigned char*) str.c_str());
-    Rc4 *rc5 = new Rc4("/home/robin/CLionProjects/RC4/testOut.txt", "/home/robin/CLionProjects/RC4/testOut2.txt", (unsigned char*) str.c_str());
+//    FILE *fichier = fopen("/home/robin/CLionProjects/RC4/testin.txt", "wb");
+//    fwrite("toto", sizeof(char), 4, fichier);
+
+//    Rc4 *rc4 = new Rc4("/home/robin/CLionProjects/RC4/testin.txt", "/home/robin/CLionProjects/RC4/testOut.txt", (unsigned char*) str.c_str());
 //    rc4->ksa();
 //    rc4->prga();
+//
+    Rc4 *rc5 = new Rc4("/home/robin/CLionProjects/RC4/testOut.txt", "/home/robin/CLionProjects/RC4/testOut2.txt", (unsigned char*) str.c_str());
     rc5->ksa();
     rc5->prga();
     return 0;
